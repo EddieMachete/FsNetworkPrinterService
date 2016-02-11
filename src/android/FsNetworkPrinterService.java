e1b48484b9e225ba6f497a51e6fb1af8bf2ee782
Eddie <elguardianenelsendero@hotmail.com>
2016-02-11 11:20:49 -0800

    Fixed issue introduced by plugman create script

--- src/android/FsNetworkPrinterService.java
+++ src/android/FsNetworkPrinterService.java
@@ -1,4 +1,4 @@
-package cordova-plugin-fs-network-printer-service;
+package cordova.plugin.FsNetworkPrinterService;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
