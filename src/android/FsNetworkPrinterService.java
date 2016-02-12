93da7be609233c9811d2d2c81c8789a8e859060d
Eddie <elguardianenelsendero@hotmail.com>
2016-02-11 22:16:36 -0800

    I think I finally got the basic structure right

--- src/android/FsNetworkPrinterService.java
+++ src/android/FsNetworkPrinterService.java
@@ -1,49 +1,62 @@
-package cordova.plugin.FsNetworkPrinterService;
+package cordova.plugin.fsnetworkprinterservice;
 
-import org.apache.cordova.CordovaPlugin;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
 import org.apache.cordova.CallbackContext;
-
-import org.json.JSONArray;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+//import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.simple.JSONValue;
+//import org.json.JSONObject;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class FsNetworkPrinterService extends CordovaPlugin {
+    private static final String TAG = "FsNetworkPrinterService";
 
+	@Override
+	public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+		Log.v(TAG, "initialization");
+		super.initialize(cordova, webView);
+	}
+
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false otherwise.
+     */
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("getNetworkPrinters")) {
-            //String message = args.getString(0);
-            //this.getNetworkPrinters(message, callbackContext);
-            this.getNetworkPrinters(callbackContext);
+    public boolean execute(final String action, final CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+        Log.v(TAG, "Executing action: " + action);
+
+        if ("echo".equals(action)) {
+            this.echo("message for echo", callbackContext);
             return true;
         }
 
-        return false;
+		callbackContext.success("did not match");
+		return true;
+
+        //return false;
     }
+    
+    private void echo(final String message, final CallbackContext callbackContext) {
+        /*if (message != null && message.length() > 0) { 
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }*/
 
-    private void getNetworkPrinters(CallbackContext callbackContext) {
-        //if (message != null && message.length() > 0) {
-        //    callbackContext.success(message);
-        //} else {
-        //    callbackContext.error("Expected one non-empty string argument.");
-        //}
-
-		JSONObject printer1 = new JSONObject();
-        returnObject.put("name", "Printer 1");
-		
-		JSONObject printer2 = new JSONObject();
-        returnObject.put("name", "Printer 2");
-		
-		JSONArray printers = new JSONArray();
-		printers.put(printer1);
-		printers.put(printer2);
-
-		//String jsonString = JSONValue.toJSONString(printers);
-		//callbackContext.success(jsonString);
-		callbackContext.success(printers);
+		callbackContext.success("abcde");
     }
 }
--- www/FsNetworkPrinterService.js
+++ www/FsNetworkPrinterService.js
@@ -1,6 +1,17 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    //exec(success, error, "FsNetworkPrinterService", "getNetworkPrinters", [arg0]);
-    exec(success, error, "FsNetworkPrinterService", "getNetworkPrinters");
+//exports.coolMethod = function (arg0, success, error) {
+//    exec(success, error, "FsNetworkPrinterService", "getNetworkPrinters", [arg0]);
+//    exec(success, error, "FsNetworkPrinterService", "echo", [arg0]);
+//    //exec(success, error, "FsNetworkPrinterService", "getNetworkPrinters");
+//};
+
+var fsNetworkPrintersService = {
+    // The JavaScript portion of a plugin always uses the cordova.exec method as follows:
+    // exec(<successFunction>, <failFunction>, <service>, <action>, [<args>]);
+    echo: function (message, success, error) {
+        exec(success, error, "FsNetworkPrinterService", "echo", [message]);
+    }
 };
+
+module.exports = fsNetworkPrintersService;
